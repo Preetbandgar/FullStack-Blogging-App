612e16b547485703f7a7634144123590c5d462b1
Pritam Bandgar <157994717+Preetbandgar@users.noreply.github.com>
2024-11-30 23:09:02 +0530

    Update output.tf

--- EKS_Terraform/output.tf
+++ EKS_Terraform/output.tf
@@ -1,3 +1,19 @@
+output "vpc_id" {
+  value = aws_vpc.Pritam_vpc.id
+}
+
+output "subnet_ids" {
+  value = aws_subnet.Pritam_subnet[*].id
+}
+
+output "internet_gateway_id" {
+  value = aws_internet_gateway.Pritam_igw.id
+}
+
+output "route_table_id" {
+  value = aws_route_table.Pritam_route_table.id
+}
+
 output "cluster_id" {
   value = aws_eks_cluster.Pritam.id
 }
@@ -6,10 +22,14 @@ output "node_group_id" {
   value = aws_eks_node_group.Pritam.id
 }
 
-output "vpc_id" {
-  value = aws_vpc.Pritam_vpc.id
+output "security_group_ids" {
+  value = [aws_security_group.Pritam_cluster_sg.id, aws_security_group.Pritam_node_sg.id]
 }
 
-output "subnet_ids" {
-  value = aws_subnet.Pritam_subnet[*].id
+output "eks_role_arn" {
+  value = aws_iam_role.Pritam_cluster_role.arn
+}
+
+output "node_role_arn" {
+  value = aws_iam_role.Pritam_node_group_role.arn
 }
